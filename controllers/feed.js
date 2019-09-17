5381205314892bbe3890a8e2b469d8de66476fe2
Nico <nico@skubos.com>
2019-09-17 12:04:38 +0700

    adds initial response

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,19 @@
 exports.getPosts = (req, res, next) => {
-  
+  res.status(200).json({ 
+    title: 'first post!', content: 'this is the first post!'
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  // Create post in database
+  res.status(201).json({
+    message: 'post created successfully!',
+    post: { 
+      id: new Date().toISOString(), 
+      title: title, 
+      content: content
+    }
+  });
 };
