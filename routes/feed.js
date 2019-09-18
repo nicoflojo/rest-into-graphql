46ea40289591c4c53b58d67f722ffe2630ea6fa1
Nico <nico@skubos.com>
2019-09-18 10:19:42 +0700

    adds post editing

--- routes/feed.js
+++ routes/feed.js
@@ -10,8 +10,15 @@ router.get('/posts', feedController.getPosts);
 router.post('/post', [
   body('title').trim().isLength({ min: 5 }),
   body('content').trim().isLength({ min: 5 })
-], feedController.createPost)
+], feedController.createPost);
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 })
+  ],
+  feedController.updatePost);
 
 module.exports = router;
